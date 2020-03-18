51cb78c8b0020a118f3f93aacfac0c8ab8cafd57
ritesh363137 <cubexo.ritesh@gmail.com>
2020-03-19 05:08:27 +0530

    added routing

---
+++ src/app/tabs/tabs.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss']
+})
+export class TabsPage {
+
+  constructor(private router: Router) {}
+
+  category() {
+    this.router.navigate(['tabs/category']);
+  }
+  offer() {
+    this.router.navigate(['tabs/offer']);
+  }
+  cart() {
+    this.router.navigate(['tabs/cart']);
+  }
+  profile() {
+    this.router.navigate(['tabs/profile']);
+  }
+
+}

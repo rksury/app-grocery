ea78170cdc6faf00639e29deda08b2d8c1622d2c
ritesh363137 <cubexo.ritesh@gmail.com>
2020-03-19 19:40:18 +0530

    all form submit

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  submitform = new FormGroup({
+        email: new FormControl(''),
+        password: new FormControl(''),
+
+   });
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
+
+   onSubmit() {
+     console.warn(this.submitform.value);
+   }
 
 }
--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+ submitform = new FormGroup({
+        name: new FormControl(''),
+        email: new FormControl(''),
+        number: new FormControl(''),
+        password: new FormControl(''),
+        confirm_psw: new FormControl(''),
 
+   });
   constructor() { }
 
   ngOnInit() {
   }
-
+    onSubmit() {
+         console.warn(this.submitform.value);
+    }
 }

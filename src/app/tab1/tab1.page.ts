acfb4e568ddc3716bca0a72fb2dbcb956e2843ef
ayush3298 <abairagi311@gmail.com>
2020-03-22 22:16:45 +0530

    added style to categories

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HomeService} from './home.service';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    categories: {}
 
-  constructor() {}
+    constructor(private homeService: HomeService) {
+    }
+
+    ionViewWillEnter() {
+        this.get_categories();
+    }
+
+    get_categories() {
+        this.homeService.getCategories().subscribe(data => {
+            this.categories = data;
+        });
+    }
 
 }

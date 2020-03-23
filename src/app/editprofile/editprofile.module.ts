5650974c9e6740b5ed6bbb72d49db62eb45b3ae5
ritesh363137 <cubexo.ritesh@gmail.com>
2020-03-23 17:30:39 +0530

    submit edit form

--- src/app/editprofile/editprofile.module.ts
+++ src/app/editprofile/editprofile.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +9,13 @@ import { EditprofilePageRoutingModule } from './editprofile-routing.module';
 import { EditprofilePage } from './editprofile.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    EditprofilePageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        EditprofilePageRoutingModule,
+        ReactiveFormsModule
+    ],
   declarations: [EditprofilePage]
 })
 export class EditprofilePageModule {}
--- src/app/editprofile/editprofile.page.ts
+++ src/app/editprofile/editprofile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-editprofile',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editprofile.page.scss'],
 })
 export class EditprofilePage implements OnInit {
+submitform = new FormGroup({
+        fullName: new FormControl('123456'),
+        userName: new FormControl(''),
+        number: new FormControl(''),
+        email: new FormControl(''),
+        shipping: new FormControl(''),
 
+   });
   constructor() { }
 
   ngOnInit() {
   }
+  onSubmit() {
+     console.warn(this.submitform.value);
+   }
 
 }

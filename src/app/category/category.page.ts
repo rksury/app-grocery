b4d5c78a0ce79849bdaa58ecc7b0a3be56354b6c
ayush3298 <abairagi311@gmail.com>
2020-03-25 02:20:03 +0530

    category page

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoryService} from './category.service';
+import {NavigationExtras, Router} from "@angular/router";
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.page.html',
-  styleUrls: ['./category.page.scss'],
+    selector: 'app-category',
+    templateUrl: './category.page.html',
+    styleUrls: ['./category.page.scss'],
 })
 export class CategoryPage implements OnInit {
+    categories: {}
 
-  constructor() { }
+    constructor(private categoryService: CategoryService,
+                private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ionViewWillEnter() {
+        this.get_categories();
+    }
+
+    ngOnInit() {
+    }
+
+    get_categories() {
+        this.categoryService.getCategories().subscribe(data => {
+            this.categories = data;
+        });
+    }
+
+    get_subcategory_products(pk) {
+        const params = {category: pk}
+        const navigationExtras: NavigationExtras = {
+                queryParams: {
+                    special: JSON.stringify(params)
+                }
+            }
+        ;
+        this.router.navigate(['/tabs/products'], navigationExtras);
+    }
+
 
 }

de02132263adc4e83d23fa7483ee2c3cb7b8c2a6
ritesh suryavanshi <cubexo.ritesh@gmail.com>
2020-03-24 20:31:04 +0530

    update home

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  base_url : 'http://127.0.0.1:8000/'
-  // base_url : 'https://groceryappbackend.herokuapp.com/'
+  // base_url : 'http://127.0.0.1:8000/'
+  base_url : 'https://groceryappbackend.herokuapp.com/'
 };
 
 /*

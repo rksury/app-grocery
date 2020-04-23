b673a6250ef17f48ae2920357fda971ae166a9e6
devloper <rksury@gmail.com>
2020-04-23 05:53:12 +0530

    change ui on home or product and share button

--- src/app/product-info/product-info.page.ts
+++ src/app/product-info/product-info.page.ts
@@ -75,7 +75,7 @@ export class ProductInfoPage implements OnInit {
             try {
                 this.utils.presentToast(error.error.error[0]);
             } catch (e) {
-                this.utils.presentToast('Some Error Occurred');
+                // this.utils.presentToast('Some Error Occurred');
 
             }
             if (error.status === 401) {
@@ -92,11 +92,11 @@ export class ProductInfoPage implements OnInit {
             try {
                 this.utils.presentToast(error.error.error[0]);
             } catch (e) {
-                this.utils.presentToast('Some Error Occurred');
+                // this.utils.presentToast('Some Error Occurred');
 
             }
             if (error.status === 401) {
-                 // this.utils.presentToast('you Have to login first');
+                this.utils.presentToast('you Have to login first');
                 this.router.navigate(['tabs/login']);
             }
         });
